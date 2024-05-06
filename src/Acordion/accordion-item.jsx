1f4653abd3bad03d3449f9ed27df60e8da9db911
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-06 18:51:33 +0530

    Icon change on button click

--- src/Acordion/accordion-item.jsx
+++ src/Acordion/accordion-item.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
+import './accordion-item.css';
+import IconMinus from '../assets/images/icon-minus.svg';
+import IconPlus from '../assets/images/icon-plus.svg';
 
 function AccordionItem({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
+  // Define state to hold the image source
+  const [imageSrc, setImageSrc] = useState(IconMinus);
+   // Function to change the image source
+   const changeImage = () => {
+    // Update the image source based on some condition or variable
+    if (isOpen) {
+      setImageSrc(IconMinus);
+    } else {  
+      setImageSrc(IconPlus);
+    }
+  };
 
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
+    changeImage();
   };
 
   return (
     <div className="accordion-item">
       <div className="accordion-header" onClick={toggleAccordion}>
-        <h2>{title}</h2>
-        <span>{isOpen ? '-' : '+'}</span>
+        <span className='title'>{title}</span>
+        <span className='open-close'>
+        <img src={imageSrc} alt="Open-Close" />
+        </span>
       </div>
       {isOpen && <div className="accordion-content">{content}</div>}
     </div>
--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-
-import './App.css';
-import Accordion from './Acordion/accordion';
+import "./App.css";
+import Accordion from "./Acordion/accordion";
+import logo from "./assets/images/icon-star.svg";
 
 function App() {
   const accordionItems = [
-    { title: 'Accordion Item 1', content: 'Content for Accordion Item 1' },
-    { title: 'Accordion Item 2', content: 'Content for Accordion Item 2' },
-    { title: 'Accordion Item 3', content: 'Content for Accordion Item 3' },
+    {
+      title: "What is Frontend Mentor, and how will it help me?",
+      content:
+        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    },
+    {
+      title: " Is Frontend Mentor free?",
+      content:
+        "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+    },
+    {
+      title: "Can I use Frontend Mentor projects in my portfolio?",
+      content:
+        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers! How can I get help if I'm stuck on a Frontend Mentor challenge? The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+    },
   ];
   return (
     <div className="App">
-      <img className='image' src="./assets/images/background-pattern-mobile.svg" alt='Image' />
-      <Accordion items={accordionItems} />
+      <img
+        className="image"
+        src="./assets/images/background-pattern-mobile.svg"
+        alt="Image"
+      />
+      <div className="accordion-container">
+        <div className="faq-heading">
+          <img className="logo" src={logo} />
+          FAQs
+        </div>
 
-  FAQs
-
-What is Frontend Mentor, and how will it help me?
-
-Frontend Mentor offers realistic coding challenges to help developers improve their 
-frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
-all levels and ideal for portfolio building.
-
-Is Frontend Mentor free?
-
-Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
-option providing access to a range of projects suitable for all skill levels.
-
-Can I use Frontend Mentor projects in my portfolio?
-
-Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-way to showcase your skills to potential employers!
-
-How can I get help if I'm stuck on a Frontend Mentor challenge?
-
-The best place to get help is inside Frontend Mentor's Discord community. There's a help 
-channel where you can ask questions and seek support from other community members.
+        <Accordion items={accordionItems} />
+      </div>
     </div>
   );
 }
