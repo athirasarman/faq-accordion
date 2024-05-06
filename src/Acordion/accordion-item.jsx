55f40684f49f4502070203589aa03558b5500b72
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-06 19:14:36 +0530

    alignment fix

--- src/Acordion/accordion-item.jsx
+++ src/Acordion/accordion-item.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import './accordion-item.css';
-import IconMinus from '../assets/images/icon-minus.svg';
-import IconPlus from '../assets/images/icon-plus.svg';
+import React, { useState } from "react";
+import "./accordion-item.css";
+import IconMinus from "../assets/images/icon-minus.svg";
+import IconPlus from "../assets/images/icon-plus.svg";
 
 function AccordionItem({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
   // Define state to hold the image source
   const [imageSrc, setImageSrc] = useState(IconMinus);
-   // Function to change the image source
-   const changeImage = () => {
+  // Function to change the image source
+  const changeImage = () => {
     // Update the image source based on some condition or variable
     if (isOpen) {
       setImageSrc(IconMinus);
-    } else {  
+    } else {
       setImageSrc(IconPlus);
     }
   };
@@ -25,9 +25,9 @@ function AccordionItem({ title, content }) {
   return (
     <div className="accordion-item">
       <div className="accordion-header" onClick={toggleAccordion}>
-        <span className='title'>{title}</span>
-        <span className='open-close'>
-        <img src={imageSrc} alt="Open-Close" />
+        <span className="title">{title}</span>
+        <span className="open-close">
+          <img src={imageSrc} alt="Open-Close" />
         </span>
       </div>
       {isOpen && <div className="accordion-content">{content}</div>}
--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
     },
     {
-      title: " Is Frontend Mentor free?",
+      title: "Is Frontend Mentor free?",
       content:
         "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
     },
